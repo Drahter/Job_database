1845fb8611a4641eb4e085f4ee81eb9afde15ee7
Drahter <Denegniy1@gmail.com>
2024-08-21 10:46:39 +0500

    added docstrings

--- src/database_classes.py
+++ src/database_classes.py
@@ -13,6 +13,7 @@ class DBCreator(AbstractDB):
         self.params = params
 
     def create_database(self):
+        """Метод удаляет старые данные, если они имеются, и создает новую базу данных с указанным в классе названием"""
         print('Создаём базу данных...')
         conn = psycopg2.connect(dbname='postgres', **self.params)
         conn.autocommit = True
@@ -24,6 +25,7 @@ class DBCreator(AbstractDB):
         conn.close()
 
     def create_tables(self):
+        """Метод создает две таблицы для их дальнейшего заполнения"""
         print('Создаём таблицы...')
         conn = psycopg2.connect(dbname=self.database_name, **self.params)
 
@@ -53,6 +55,7 @@ class DBCreator(AbstractDB):
         conn.close()
 
     def fill_in_tables(self, emp_data, vac_data):
+        """Метод заполняет таблицы БД данными, которые в него передаются в виде списка словарей"""
         print('Вносим данные в таблицы...')
         conn = psycopg2.connect(dbname=self.database_name, **self.params)
 
@@ -92,6 +95,7 @@ class DBManager:
         self.cur = self.conn.cursor()
 
     def get_companies_and_vacancies_count(self):
+        """Метод выводит таблицу всех работодателей и количество их вакансий"""
         self.cur.execute("""
         SELECT employers.employer_name, COUNT(vacancies.employer_id)
         FROM employers
@@ -103,6 +107,7 @@ class DBManager:
         return self.cur.fetchall()
 
     def get_all_vacancies(self):
+        """Метод выводит данные по всем вакансиям, сортируя их по работодателям"""
         self.cur.execute("""
         SELECT employers.employer_name, vacancy_name, salary, vacancy_url
         FROM vacancies
@@ -112,12 +117,14 @@ class DBManager:
         return self.cur.fetchall()
 
     def get_avg_salary(self):
+        """Метод для получения средней зарплаты среди всех вакансий в базе"""
         self.cur.execute("""
         SELECT avg(salary) from vacancies
         """)
         return self.cur.fetchall()
 
     def get_vacancies_with_higher_salary(self):
+        """Метод возвращает все вакансии, зарплата которых превышает среднюю"""
         self.cur.execute("""
         SELECT vacancy_name, salary
         FROM vacancies
@@ -128,6 +135,7 @@ class DBManager:
         return self.cur.fetchall()
 
     def get_vacancies_with_keyword(self, keyword):
+        """Метод возвращает вакансии по ключевому слову, которое указывает пользователь"""
         request_sql = """
         SELECT * FROM vacancies
         WHERE LOWER (vacancy_name) LIKE %s
@@ -136,5 +144,6 @@ class DBManager:
         return self.cur.fetchall()
 
     def close_connection(self):
+        """Метод для закрытия соединения с БД при завершении работы программы"""
         print('Спасибо за использование данной программы!')
         self.conn.close()
